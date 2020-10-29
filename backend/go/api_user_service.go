f3402abca0013aeebacc00ec5cfa2f1a061b5c46
ForeverNever <gennadi.stamm@googlemail.com>
2020-10-29 18:05:37 +0100

    go service mock

--- backend/go/api_user_service.go
+++ backend/go/api_user_service.go
@@ -9,12 +9,8 @@
 
 package openapi
 
-import (
-	"errors"
-)
-
 // UserApiService is a service that implents the logic for the UserApiServicer
-// This service should implement the business logic for every endpoint for the UserApi API. 
+// This service should implement the business logic for every endpoint for the UserApi API.
 // Include any external packages or services that will be required by this service.
 type UserApiService struct {
 }
@@ -28,26 +24,26 @@ func NewUserApiService() UserApiServicer {
 func (s *UserApiService) CreateUser(user User) (interface{}, error) {
 	// TODO - update CreateUser with the required logic for this service method.
 	// Add api_user_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
-	return nil, errors.New("service method 'CreateUser' not implemented")
+	return user, nil
 }
 
 // DeleteUser - Deletes a User
 func (s *UserApiService) DeleteUser(id string) (interface{}, error) {
 	// TODO - update DeleteUser with the required logic for this service method.
 	// Add api_user_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
-	return nil, errors.New("service method 'DeleteUser' not implemented")
+	return User{Id: id, Nickname: "Deleted-Dummy"}, nil
 }
 
 // GetUser - Get a User
 func (s *UserApiService) GetUser(id string) (interface{}, error) {
 	// TODO - update GetUser with the required logic for this service method.
 	// Add api_user_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
-	return nil, errors.New("service method 'GetUser' not implemented")
+	return User{Id: id, Nickname: "Get-Dummy"}, nil
 }
 
 // UpdateUser - Updates a User
 func (s *UserApiService) UpdateUser(user User) (interface{}, error) {
 	// TODO - update UpdateUser with the required logic for this service method.
 	// Add api_user_service.go to the .openapi-generator-ignore to avoid overwriting this service implementation when updating open api generation.
-	return nil, errors.New("service method 'UpdateUser' not implemented")
+	return user, nil
 }
